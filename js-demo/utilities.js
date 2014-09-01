e6c4ade41f4a6365fa7186bb58fe95f57f643717
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2014-09-01 10:38:35 +0200

    Array comparison functions

--- js-demo/utilities.js
+++ js-demo/utilities.js
@@ -1,6 +1,49 @@
 
 // List utilities
 
+function array_cmp(a, b) {
+    if (a instanceof Array) {
+        if (b instanceof Array) {
+            var alen = a.length, blen = b.length;
+            if (alen < blen) return -1;
+            if (alen > blen) return 1;
+            for (var i = 0; i < alen; i++) {
+                var cmp = array_cmp(a[i], b[i]);
+                if (cmp) return cmp;
+            }
+            return 0;
+        } else {
+            return 1;
+        }
+    } else if (b instanceof Array) {
+        return -1;
+    } else {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    }
+}
+
+function array_eq(a, b) {
+    if (a instanceof Array) {
+        if (b instanceof Array) {
+            var alen = a.length, blen = b.length;
+            if (alen !== blen) return false;
+            for (var i = 0; i < alen; i++) {
+                var eq = array_eq(a[i], b[i]);
+                if (!eq) return eq;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    } else if (b instanceof Array) {
+        return false;
+    } else {
+        return a == b;
+    }
+}
+
 function flatten(obj) {
     if (obj instanceof Array) {
         var list = [];
