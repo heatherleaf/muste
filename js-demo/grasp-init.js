e5e0a6df542030af0307ae6130abff69ef4e06fd
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2014-09-01 14:33:33 +0200

    Wrap it all together

---
+++ js-demo/grasp-init.js
@@ -0,0 +1,11 @@
+
+var ConnectedTrees = false; 
+
+var IncludeAbstract = true;
+
+var Grammar = Grasp;
+var Languages = ["GraspSwe", "GraspEng", "GraspGer"];
+var StartCat = 'Start';
+
+var MapWordsToHTML = {'GraspZbl': map_words_to_images};
+var Metadata = {'GraspZbl': BlissMetadata};
